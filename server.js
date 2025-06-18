3dff568f5c651e083b02283dc29a8f002c56aa90
Nikhil Kumar Gupta <118952235+nkgup2349@users.noreply.github.com>
2025-06-18 18:08:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,12 +5,6 @@ import connectDB from './config/mongodb.js'
 import userRouter from './routes/routes.js'
 import imageRouter from './routes/imageRoutes.js'
 
-import path from 'path'                          
-import { fileURLToPath } from 'url'
-
-const __filename = fileURLToPath(import.meta.url)     
-const __dirname = path.dirname(__filename)  
-
 const PORT = process.env.PORT || 4000 ; 
 
 const app = express();
@@ -23,8 +17,6 @@ await connectDB()
 app.use('/api/user' , userRouter)
 app.use('/api/image' , imageRouter)
 
-app.use(express.static(path.join(__dirname, 'dist'))) 
-
 app.get('/', (req, res)=>{
     return res.send("API WORKING")
 })
@@ -32,8 +24,8 @@ app.get('/newapi', (req, res)=>{
     return res.send("Are are new api WORKING")
 })
 
-app.get('*', (req, res) => {        
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
-})
+app.get('*', (req, res) => {
+  res.redirect('/');
+});
 
 app.listen(PORT, ()=>console.log("Server running on port " + PORT))
